08292c371a5ebfba33a612ba3b1a3587635d9af1
Nikolaj Majorov <nikolaj@majorov.biz>
2014-08-18 23:37:40 +0200

    correct the tests

---
+++ rest-consumer-example/src/main/java/biz/majorov/camel/incident/IncidentServiceImpl.java
@@ -0,0 +1,81 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package biz.majorov.camel.incident;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+import java.util.HashMap;
+import java.util.Map;
+
+import java.util.Random;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Interface with the services we want to expose as web services using code first.
+ * <p/>
+ * This is a basic example, you can use the JAX-WS annotations to control the contract.
+ */
+
+@Path("/incidents/")
+public class IncidentServiceImpl implements IncidentService{
+     private static final Logger LOG = LoggerFactory.getLogger(IncidentServiceImpl.class);
+      long LOWER_RANGE = 0; //assign lower range value
+ 	 long UPPER_RANGE = 1000000; //assign upper range value
+ 	 Random random = new Random();
+
+ 	Map<Long, Incident> storage = new HashMap<Long, Incident>();
+
+     public IncidentServiceImpl(){}
+
+    /**
+     * Operation to report an incident
+     */
+    public Response reportIncident(Incident incident){
+
+        long randomValue = LOWER_RANGE + 
+                           (long)(random.nextDouble()*(UPPER_RANGE - LOWER_RANGE));
+        incident.setId(randomValue);
+        storage.put(randomValue, incident);
+         LOG.info("report new incident with id : " + randomValue);
+        return Response.ok().build();
+    }
+
+    /**
+     * Operation to get an incident
+     */
+
+    public Response getIncident(String id){
+        LOG.info("get incident by: " + id);
+        Response r;
+        Long longId = Long.parseLong(id);
+        Incident incident = storage.get(longId);
+        
+        if (incident == null){
+             LOG.info("incident not found for id: " + id);
+        	r =  Response.status(Response.Status.NOT_FOUND).build();
+        }else{
+             LOG.info("found incident "+ incident +"  for id: " + id);
+        	r =Response.ok(incident).build();
+        }
+        return r;
+    }
+}
--- rest-consumer-example/src/test/java/biz/majorov/camel/CamelTest.java
+++ rest-consumer-example/src/test/java/biz/majorov/camel/CamelTest.java
@@ -4,15 +4,13 @@ import biz.majorov.camel.incident.Incident;
 import biz.majorov.camel.incident.IncidentService;
 import org.apache.camel.test.blueprint.CamelBlueprintTestSupport;
 import org.apache.cxf.jaxrs.client.JAXRSClientFactory;
+import org.apache.camel.util.FileUtil;
 
 import javax.ws.rs.core.Response;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
-
-
-import static org.junit.Assert.*;
+import java.io.File;
 
 
 /**
@@ -20,10 +18,8 @@ import static org.junit.Assert.*;
  *
  * @author Nikolaj Majorov
  */
-@Ignore
 public class CamelTest extends CamelBlueprintTestSupport {
-    private static final String ROUTER_ADDRESS = "http://localhost:8181/cxf/inc/incident";
-
+    private static final String ROUTER_ADDRESS = "http://localhost:8181/cxf/inc/incidents";
 
 
     @Override
@@ -32,12 +28,27 @@ public class CamelTest extends CamelBlueprintTestSupport {
     }
 
     @Test
-    public void testPublishedEndpointUrl(){
-       IncidentService service =  JAXRSClientFactory.create(ROUTER_ADDRESS, IncidentService.class);
-
-       Incident incident = new Incident();
+    public void testPublishedEndpointUrl() throws Exception {
+        IncidentService service = JAXRSClientFactory.create(ROUTER_ADDRESS, IncidentService.class);
+        Incident incident = new Incident();
         incident.setDescription("test1 description");
-        Response response =  service.reportIncident(incident) ;
-        assertEquals(response.getStatus(), Response.Status.OK);
+        Response response = service.reportIncident(incident);
+        assertEquals(response.getStatus(), Response.Status.OK.getStatusCode());
+    }
+
+
+    @Test
+    public void testRoute() {
+
+        try {
+            File incidentFile = new File(getClass().getClassLoader().getResource("incident.xml").toURI());
+            File currentDir = new File(".");
+            File destinationFile = new File(currentDir.getPath() + "/work/inc/input/incident.xml");
+            log.info("get new incident file: " + incidentFile);
+            FileUtil.copyFile(incidentFile, destinationFile);
+        } catch (Exception ex) {
+            log.error("route test error", ex);
+            fail();
+        }
     }
 }
